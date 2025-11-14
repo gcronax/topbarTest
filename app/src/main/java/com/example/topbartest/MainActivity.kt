bebb0b18cb893d78816f5b18eeab993f6ad78707
guicamher <guillecampher15@gmail.com>
2025-11-14 20:52:36 +0100

    a

--- app/src/main/java/com/example/topbartest/MainActivity.kt
+++ app/src/main/java/com/example/topbartest/MainActivity.kt
@@ -5,17 +5,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.rememberLazyStaggeredGridState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -35,12 +46,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.topbartest.ui.theme.TopbarTestTheme
+import com.example.topbartest.ui.theme.primary
+import kotlin.text.contains
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -56,7 +71,11 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize(),
                     topBar = {
                         CenterAlignedTopAppBar(
-                            title = { Text(text = tituloPasado) },
+                            title = { Text(text = tituloPasado)
+                                    },
+                            actions = {
+                                IconDropDownMenu(modifier = Modifier)
+                            },
                             colors= TopAppBarDefaults.topAppBarColors(
                                 containerColor = MaterialTheme.colorScheme.primary,
                                 titleContentColor = MaterialTheme.colorScheme.onPrimaryContainer,
@@ -64,7 +83,7 @@ class MainActivity : ComponentActivity() {
                         )
                     },
                     floatingActionButton = {
-                        if (show){
+
                             FloatingActionButton(
                                 onClick = { show=false
                                     navController.navigate("Principal")
@@ -73,7 +92,7 @@ class MainActivity : ComponentActivity() {
                             ) {
                                 Icon(Icons.AutoMirrored.Filled.ArrowBack, "Floating action button.")
                             }
-                        }
+
 
                     }
                 )
@@ -92,43 +111,160 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+data class imgenes(
 
+    val direccion: Int,
+    val nombre: String
+
+)
 @Composable
 fun principal(navController: NavHostController, modifier: Modifier = Modifier,
               titulo: (String) -> Unit,show: (Boolean) -> Unit) {
-    Column (
-        modifier=modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-        ){
-        TextButton(onClick = {titulo("Información")
-                                show(true)
-            navController.navigate("Secundario")
-                             }, modifier = Modifier
-        ) {
-            Text("Información")
-        }
-        TextButton(onClick = {titulo("Administración")
-                                show(true)
-            navController.navigate("Secundario")
+    val gridState = rememberLazyStaggeredGridState()
 
-        }, modifier = Modifier
-        ) {
-            Text("Administración")
-        }
-        TextButton(onClick = { titulo("Comercio")
-                                show(true)
-            navController.navigate("Secundario")
+    val boxes by remember {
+        mutableStateOf(listOf(
+            imgenes(
+                R.drawable.image,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image1,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image2,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image3,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image4,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image5,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image6,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image7,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image8,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image1,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image2,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image3,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image4,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image5,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image6,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image7,
+                "cerdo"
+            ),
+            imgenes(
+                R.drawable.image8,
+                "cerdo"
+            ),
 
-        }, modifier = Modifier
-        ) {
-            Text("Comercio")
-        }
-
+        ))
     }
+    LazyVerticalStaggeredGrid(
+        modifier= Modifier.fillMaxSize(),
+        columns = StaggeredGridCells.Fixed(2),
+        state = gridState,
+        content = {
+            items(boxes.size) { indice ->
+                    Box(
+                        modifier = Modifier.padding(5.dp)
+                    ) {
+
+                        Image(
+                            painter = painterResource(boxes[indice].direccion),
+                            contentDescription = null
+                        )
+                        Text(
+                            text = boxes[indice].nombre,
+                            modifier = Modifier.padding(5.dp)
+                        )
+                    }
+
+            }
+        })
 }
 @Composable
 fun secundario(navController: NavHostController, modifier: Modifier = Modifier) {
 
 }
 
+@Composable
+fun IconDropDownMenu(modifier: Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(Modifier.padding(20.dp)) {
+        IconButton(onClick = {
+            expanded = true
+        }) {
+            Icon(imageVector = Icons.Filled.MoreVert,
+                contentDescription = "Buscar",
+                tint= primary
+            )
+        }
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+//            modifier = modifier.align(Alignment.End)
+        ) {
+            DropdownMenuItem(
+                onClick = { expanded = false },
+                leadingIcon ={
+                    Icon(imageVector = Icons.Filled.Share,
+                        contentDescription = "Compartir")
+                },
+                text = {Text(text = "Compartir")}
+
+            )
+            DropdownMenuItem(
+                onClick = { expanded = false },
+                leadingIcon ={
+                    Icon(imageVector = Icons.Filled.Lock,
+                        contentDescription = "Compartir")
+                },
+                text = {Text(text = "Album")},
+
+                )
+
+        }
+    }
+}
--- app/src/main/java/com/example/topbartest/ui/theme/Color.kt
+++ app/src/main/java/com/example/topbartest/ui/theme/Color.kt
@@ -2,8 +2,8 @@ package com.example.topbartest.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+val primary = Color(0xFF005CB2)
+val secondary = Color(0xFF6AB7FF)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
